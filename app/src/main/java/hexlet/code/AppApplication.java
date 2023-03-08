7f5600de63d859efa99279d708e4b94d7f327d11
redgradient <neutral1shade@yahoo.com>
2023-03-08 15:27:46 +0400

    remove commented code

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +9,6 @@ public class AppApplication {
 
     public static void main(String[] args) {
          SpringApplication.run(AppApplication.class, args);
-//        SpringApplication application = new SpringApplication(AppApplication.class);
-//        application.setBannerMode(Banner.Mode.OFF);
-//        application.run(args);
     }
 
 }

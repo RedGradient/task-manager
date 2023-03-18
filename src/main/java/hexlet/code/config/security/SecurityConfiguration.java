b7ef96593aba35c84be7f03bf5c7bf3e65fa6ac8
redgradient <neutral1shade@yahoo.com>
2023-03-19 02:24:09 +0400

    enable @PreAuthorize rules

--- src/main/java/hexlet/code/config/security/SecurityConfiguration.java
+++ src/main/java/hexlet/code/config/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Autowired

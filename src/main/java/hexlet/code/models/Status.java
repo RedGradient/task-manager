3faf5878f3d9f8ad1e713d9133004cb7ffef0323
redgradient <neutral1shade@yahoo.com>
2023-03-19 02:24:22 +0400

    add name field

--- src/main/java/hexlet/code/models/Status.java
+++ src/main/java/hexlet/code/models/Status.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "statuses")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Status extends BaseModel {
-
+    private String name;
 }

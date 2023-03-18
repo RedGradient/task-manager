b13b0b64e1c06ac35ceaa1ff91da97e07935de0b
redgradient <neutral1shade@yahoo.com>
2023-03-19 02:25:07 +0400

    add StatusNotFoundException.java

---
+++ src/main/java/hexlet/code/StatusNotFoundException.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class StatusNotFoundException extends RuntimeException {
+    public StatusNotFoundException(Long id) {
+        super("Status with id " + id + " not found");
+    }
+}

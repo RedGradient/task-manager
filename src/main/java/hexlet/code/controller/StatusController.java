d7f5a6e5836ffd2fcf3c601ff22ee1b95ec26761
redgradient <neutral1shade@yahoo.com>
2023-03-19 02:38:49 +0400

    fix checkstyle errors

--- src/main/java/hexlet/code/controller/StatusController.java
+++ src/main/java/hexlet/code/controller/StatusController.java
@@ -3,16 +3,26 @@ package hexlet.code.controller;
 import hexlet.code.dto.StatusDto;
 import hexlet.code.models.Status;
 import hexlet.code.service.StatusService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/statuses")
 public class StatusController {
 
-    private final String ID = "/id";
-    private final String AUTHENTICATED = "isAuthenticated()";
+    private static final String ID = "/id";
+    private static final String AUTHENTICATED = "isAuthenticated()";
 
     @Autowired
     private StatusService statusService;

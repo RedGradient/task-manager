9cad17c8c8fdc06df9e93583391bfc01476275b1
redgradient <neutral1shade@yahoo.com>
2023-03-20 19:58:19 +0400

    add all args constructor

--- src/main/java/hexlet/code/dto/StatusDto.java
+++ src/main/java/hexlet/code/dto/StatusDto.java
@@ -1,10 +1,12 @@
 package hexlet.code.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class StatusDto {
     private String name;
 }

bcb537d6f36a6c37a8beae14e5c901f38cff70bb
redgradient <neutral1shade@yahoo.com>
2023-03-23 17:50:06 +0400

    rename field: username -> email

--- src/main/java/hexlet/code/dto/AuthenticationRequest.java
+++ src/main/java/hexlet/code/dto/AuthenticationRequest.java
@@ -12,6 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
 
-    private String username;
+    private String email;
     private String password;
 }

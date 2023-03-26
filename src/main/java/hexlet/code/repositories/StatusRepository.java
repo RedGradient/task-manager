d9855e4be1a7fca3c77993015857ef6e8e79aeb5
redgradient <neutral1shade@yahoo.com>
2023-03-26 23:59:07 +0400

    remove StatusRepository.java

--- src/main/java/hexlet/code/repositories/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package hexlet.code.repositories;
-
-import hexlet.code.models.Status;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StatusRepository  extends CrudRepository<Status, Long> {
-    Iterable<Status> findAll();
-}

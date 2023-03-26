bd5386ffbf56a55c26a727109e2376191a7bec96
redgradient <neutral1shade@yahoo.com>
2023-03-27 00:02:36 +0400

    add 'not-found-by-email' constructor

--- src/main/java/hexlet/code/UserNotFoundException.java
+++ src/main/java/hexlet/code/UserNotFoundException.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
+
     public UserNotFoundException(long id) {
         super("User " + id + " not found");
+    }
+
+    public UserNotFoundException(String email) {
+        super("User " + email + " not found");
     }
 
 }

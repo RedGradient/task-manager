fa54384b1a4f9d1f5d1106b6b32a78ac6332dbc9
redgradient <neutral1shade@yahoo.com>
2023-03-26 23:57:13 +0400

    rename Status -> TaskStatus

--- src/main/java/hexlet/code/models/TaskStatus.java
+++ src/main/java/hexlet/code/models/TaskStatus.java
@@ -3,6 +3,7 @@ package hexlet.code.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,8 @@ import lombok.Setter;
 @Table(name = "statuses")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Status extends BaseModel {
+public class TaskStatus extends BaseModel {
+    @NotNull
     @NotBlank
     private String name;
 }

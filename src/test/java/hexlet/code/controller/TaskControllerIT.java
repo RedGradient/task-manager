3c666a85c11f40138445488fb09f65461e55243a
redgradient <neutral1shade@yahoo.com>
2023-04-05 20:45:47 +0400

    add tests

--- src/test/java/hexlet/code/controller/TaskControllerIT.java
+++ src/test/java/hexlet/code/controller/TaskControllerIT.java
@@ -3,33 +3,44 @@ package hexlet.code.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import hexlet.code.config.SpringConfigForIT;
+import hexlet.code.dto.LabelDto;
 import hexlet.code.dto.TaskDto;
 import hexlet.code.dto.TaskStatusDto;
 import hexlet.code.dto.UserDto;
+import hexlet.code.models.Label;
 import hexlet.code.models.Task;
-import hexlet.code.models.User;
 import hexlet.code.repositories.TaskRepository;
 import hexlet.code.repositories.TaskStatusRepository;
 import hexlet.code.repositories.UserRepository;
+import hexlet.code.service.LabelService;
 import hexlet.code.service.TaskService;
 import hexlet.code.service.TaskStatusService;
 import hexlet.code.service.UserService;
 import hexlet.code.utils.TestUtils;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
 import static hexlet.code.controller.TaskController.ID;
-import static hexlet.code.utils.TestUtils.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static hexlet.code.utils.TestUtils.TEST_USERNAME;
+import static hexlet.code.utils.TestUtils.asJson;
+import static hexlet.code.utils.TestUtils.fromJson;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -67,16 +78,12 @@ public class TaskControllerIT {
     @Autowired
     private UserRepository userRepository;
     @Autowired
+    private LabelService labelService;
+    @Autowired
     private TestUtils utils;
-
 
     @BeforeAll
     public void beforeAll() throws Exception {
-        utils.tearDown();
-    }
-    @BeforeEach
-    public void beforeEach() throws Exception {
-        utils.tearDown();
         utils.regDefaultUser();
         userService.createNewUser(new UserDto(
                 EXECUTOR_USERNAME,
@@ -85,10 +92,14 @@ public class TaskControllerIT {
                 "12345"
         ));
     }
+    @BeforeEach
+    public void beforeEach() throws Exception {
+    }
 
     @AfterEach
     public void clear() {
-        utils.tearDown();
+        taskRepository.deleteAll();
+        statusRepository.deleteAll();
     }
 
     @Test
@@ -100,10 +111,14 @@ public class TaskControllerIT {
     public void createTask() throws Exception {
         var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
         var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var label = labelService.createLabel(new LabelDto("Feature"));
+        var labels = new HashSet<Long>();
+        labels.add(label.getId());
         var taskDto = new TaskDto(
                 "Task",
                 "Description",
                 taskStatus.getId(),
+                labels,
                 executor.getId()
         );
 
@@ -111,8 +126,15 @@ public class TaskControllerIT {
                 .content(asJson(taskDto))
                 .contentType(APPLICATION_JSON);
         var response = utils.perform(request, TEST_USERNAME)
-                .andExpect(status().isOk())
-                .andReturn();
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse();
+
+        final Task task = fromJson(
+                response.getContentAsString(), new TypeReference<>() { }
+        );
+
+        assertTrue(taskRepository.findById(task.getId()).isPresent());
     }
 
     @Test
@@ -122,12 +144,14 @@ public class TaskControllerIT {
         var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
         var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
 
-        final var tasksCountBeforeAdding = taskService.getTasksCount();
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
+
         final var newTaskCount = 3;
 
-        taskRepository.save(new Task("Task", "Descr", taskStatus, author, executor));
-        taskRepository.save(new Task("Task", "Descr", taskStatus, author, executor));
-        taskRepository.save(new Task("Task", "Descr", taskStatus, author, executor));
+        taskRepository.save(new Task("Task 1", "Descr", taskStatus, labels, author, executor));
+        taskRepository.save(new Task("Task 2", "Descr", taskStatus, labels, author, executor));
+        taskRepository.save(new Task("Task 3", "Descr", taskStatus, labels, author, executor));
         // -------
 
         var response = utils.perform(get(TASKS_CONTROLLER))
@@ -138,19 +162,21 @@ public class TaskControllerIT {
                 response.getContentAsString(), new TypeReference<>() { }
         );
 
-        assertEquals(newTaskCount + tasksCountBeforeAdding, tasks.size());
+        assertEquals(newTaskCount, tasks.size());
     }
-
 
     @Test
     public void getTaskById() throws Exception {
         var author = utils.getUserByEmail(TEST_USERNAME);
         var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
         var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
         var task = new Task(
                 "Task",
                 "Description",
                 taskStatus,
+                labels,
                 author,
                 executor
         );
@@ -169,18 +195,143 @@ public class TaskControllerIT {
     }
 
     @Test
-    public void createTaskFails() throws Exception { }
+    public void createTaskFails() throws Exception {
+        var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
+        var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var labels = new HashSet<Long>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")).getId());
+        var taskDto = new TaskDto(
+                "Task",
+                "Description",
+                taskStatus.getId(),
+                labels,
+                executor.getId()
+        );
+
+        var request = post(TASKS_CONTROLLER)
+                .content(asJson(taskDto))
+                .contentType(APPLICATION_JSON);
+        utils.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn()
+                .getResponse();
+
+    }
 
     @Test
-    public void updateTask() throws Exception { }
+    public void updateTask() throws Exception {
+        var author = utils.getUserByEmail(TEST_USERNAME);
+        var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
+        var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var taskName = "Task";
+        var oldDescr = "Old Description";
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
+        var task = new Task(
+                taskName,
+                oldDescr,
+                taskStatus,
+                labels,
+                author,
+                executor
+        );
+        taskRepository.save(task);
+
+        assertEquals(taskRepository.findById(task.getId()).get().getDescription(), oldDescr);
+
+        var newDescr = "Updated description";
+        var labelIds = new HashSet<Long>();
+        labelIds.add(labelService.createLabel(new LabelDto("Feature")).getId());
+        var newTaskDto = new TaskDto(
+                taskName,
+                newDescr,
+                taskStatus.getId(),
+                labelIds,
+                executor.getId()
+        );
+        var request = put(TASKS_CONTROLLER + ID, task.getId())
+                .content(asJson(newTaskDto))
+                .contentType(APPLICATION_JSON);
+
+        utils.perform(request, TEST_USERNAME).andExpect(status().isOk());
+
+        assertEquals(taskRepository.findById(task.getId()).get().getDescription(), newDescr);
+    }
 
     @Test
-    public void updateTaskFails() throws Exception { }
+    public void updateTaskFails() throws Exception {
+        var author = utils.getUserByEmail(TEST_USERNAME);
+        var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
+        var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var taskName = "Task";
+        var oldDescr = "Old Description";
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
+        var task = new Task(
+                taskName,
+                oldDescr,
+                taskStatus,
+                labels,
+                author,
+                executor
+        );
+        taskRepository.save(task);
+
+        assertEquals(taskRepository.findById(task.getId()).get().getDescription(), oldDescr);
+
+        var newDescr = "Updated description";
+        var labelIds = new HashSet<Long>();
+        labelIds.add(labelService.createLabel(new LabelDto("Feature")).getId());
+        var newTaskDto = new TaskDto(
+                taskName,
+                newDescr,
+                taskStatus.getId(),
+                labelIds,
+                executor.getId()
+        );
+        var request = put(TASKS_CONTROLLER + ID, task.getId())
+                .content(asJson(newTaskDto))
+                .contentType(APPLICATION_JSON);
+
+        utils.perform(request).andExpect(status().isForbidden());
+
+        assertNotEquals(taskRepository.findById(task.getId()).get().getDescription(), newDescr);
+    }
 
     @Test
-    public void deleteTask() throws Exception { }
+    public void deleteTask() throws Exception {
+        var author = utils.getUserByEmail(TEST_USERNAME);
+        var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
+        var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
+        var task = taskRepository.save(new Task(
+                "Task 1", "Descr", taskStatus, labels, author, executor));
+
+        assertEquals(1, taskRepository.count());
+
+        utils.perform(delete(TASKS_CONTROLLER + ID, task.getId()), TEST_USERNAME)
+                .andExpect(status().isOk());
+
+        assertEquals(0, taskRepository.count());
+    }
 
     @Test
-    public void deleteTaskFails() throws Exception { }
+    public void deleteTaskFails() throws Exception {
+        var author = utils.getUserByEmail(TEST_USERNAME);
+        var executor = utils.getUserByEmail(EXECUTOR_USERNAME);
+        var taskStatus = statusService.createNewStatus(new TaskStatusDto("New"));
+        var labels = new HashSet<Label>();
+        labels.add(labelService.createLabel(new LabelDto("Feature")));
+        var task = taskRepository.save(new Task(
+                "Task 1", "Descr", taskStatus, labels, author, executor));
+
+        assertEquals(1, taskRepository.count());
+
+        utils.perform(delete(TASKS_CONTROLLER + ID, task.getId()))
+                .andExpect(status().isForbidden());
+
+        assertEquals(1, taskRepository.count());
+    }
 
 }

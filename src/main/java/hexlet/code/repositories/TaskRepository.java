151a61f1c320fecef73fbb0d6c3ceb0f27411e31
redgradient <neutral1shade@yahoo.com>
2023-04-12 00:37:26 +0400

    make filtration support

--- src/main/java/hexlet/code/repositories/TaskRepository.java
+++ src/main/java/hexlet/code/repositories/TaskRepository.java
@@ -1,26 +1,34 @@
 package hexlet.code.repositories;
 
-//import com.querydsl.core.types.dsl.SimpleExpression;
-//import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.SimpleExpression;
 import hexlet.code.models.Task;
-//import hexlet.code.models.QTask;
+import hexlet.code.models.QTask;
 
-//import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-//import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-//import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TaskRepository extends
-        CrudRepository<Task, Long> {
-//        QuerydslPredicateExecutor<Task> {
-//        QuerydslBinderCustomizer<QTask> {
 
-//    @Override
-//    default void customize(QuerydslBindings bindings, QTask task) {
-//        bindings.bind(task.taskStatus).first(StringExpression::containsIgnoreCase);
-//        bindings.bind(task.executorId).first(StringExpression::eq);
-//        bindings.bind(task.labels).first(SimpleExpression::eq);
-//        bindings.bind(task.authorId).first(StringExpression::containsIgnoreCase);
-//    }
+public interface TaskRepository extends
+        CrudRepository<Task, Long>,
+        QuerydslPredicateExecutor<Task>,
+        QuerydslBinderCustomizer<QTask> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QTask task) {
+        bindings.bind(task.author).first(SimpleExpression::eq);
+        bindings.bind(task.executor).first(SimpleExpression::eq);
+        bindings.bind(task.taskStatus).first(SimpleExpression::eq);
+
+
+        bindings.bind(task.labels).first((path, value) -> {
+            BooleanBuilder predicate = new BooleanBuilder();
+            value.forEach(label -> predicate.and(path.any().eq(label)));
+            return predicate;
+        });
+
+    }
 
 }

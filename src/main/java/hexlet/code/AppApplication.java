7c048ce88641e8bf4471b67ad4c5d013cacb31b8
redgradient <neutral1shade@yahoo.com>
2023-04-12 15:28:53 +0400

    remove dead code

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -8,14 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@EnableOpenApi
-//@OpenAPIDefinition(
-//    info = @Info(
-//        title = "Task Manager API",
-//        version = "1.0",
-//        description = "CRUD API for all entities of the Task Manager"
-//    )
-//)
 public class AppApplication {
 
     public static void main(String[] args) {

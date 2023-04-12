27dbf5bbd93fc9ec26043d2763961888ec6fd92c
redgradient <neutral1shade@yahoo.com>
2023-04-12 15:29:05 +0400

    remove dead code

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,10 +1,7 @@
 package hexlet.code;
 
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.oas.annotations.EnableOpenApi;
 
 
 @SpringBootApplication

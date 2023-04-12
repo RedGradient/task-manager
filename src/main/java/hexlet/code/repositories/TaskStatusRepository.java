a4c80567dbc342d2105d6f3153a760c651dd3c3a
redgradient <neutral1shade@yahoo.com>
2023-04-12 15:22:45 +0400

    add 'findByName'

--- src/main/java/hexlet/code/repositories/TaskStatusRepository.java
+++ src/main/java/hexlet/code/repositories/TaskStatusRepository.java
@@ -3,6 +3,10 @@ package hexlet.code.repositories;
 import hexlet.code.models.TaskStatus;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TaskStatusRepository extends CrudRepository<TaskStatus, Long> {
     Iterable<TaskStatus> findAll();
+
+    Optional<TaskStatus> findByName(String name);
 }

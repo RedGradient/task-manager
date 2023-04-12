f67050221f96903afd89ca0273f5436fd8b88340
redgradient <neutral1shade@yahoo.com>
2023-04-12 15:20:27 +0400

    add 'findByName'

--- src/main/java/hexlet/code/repositories/LabelRepository.java
+++ src/main/java/hexlet/code/repositories/LabelRepository.java
@@ -3,5 +3,8 @@ package hexlet.code.repositories;
 import hexlet.code.models.Label;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface LabelRepository extends CrudRepository<Label, Long> {
+    Optional<Label> findByName(String name);
 }

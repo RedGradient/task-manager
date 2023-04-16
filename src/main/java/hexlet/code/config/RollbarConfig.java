236a9f2fde8a78d29e2536ce0d24f213691c872d
redgradient <neutral1shade@yahoo.com>
2023-04-16 20:43:29 +0400

    add @EnableWebMvc annotation

--- src/main/java/hexlet/code/config/RollbarConfig.java
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -6,10 +6,12 @@ import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
-@ComponentScan({"hexlet.code.config.rollbar", "com.rollbar.spring"})
+@EnableWebMvc
+@ComponentScan({"hexlet.code.config", "com.rollbar.spring"})
 public class RollbarConfig {
     @Bean
     public Rollbar rollbar() {

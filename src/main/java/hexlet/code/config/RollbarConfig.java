a97c3b0017e55173a63c6ebe52d46d1871ab83cc
redgradient <neutral1shade@yahoo.com>
2023-04-18 20:14:48 +0400

    get token from application.yaml

--- src/main/java/hexlet/code/config/RollbarConfig.java
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -3,25 +3,33 @@ package hexlet.code.config;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.notifier.config.Config;
 import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
-@EnableWebMvc
 @ComponentScan({"hexlet.code", "com.rollbar.spring"})
 public class RollbarConfig {
+
+    @Value("${rollbar-token:}")
+    private String rollbarToken;
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
     @Bean
     public Rollbar rollbar() {
-        var accessToken = System.getenv("ROLLBAR_TOKEN");
-        return new Rollbar(getRollbarConfigs(accessToken));
+        return new Rollbar(getRollbarConfigs(rollbarToken));
     }
 
     private Config getRollbarConfigs(String accessToken) {
         return RollbarSpringConfigBuilder.withAccessToken(accessToken)
                 .environment("development")
+                .enabled(activeProfile.equals("prod"))
                 .build();
     }
 }

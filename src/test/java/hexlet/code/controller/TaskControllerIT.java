aca4d3e5fc0b0edcc20ff0dcdd0e65198fa84b4c
redgradient <neutral1shade@yahoo.com>
2023-05-26 23:48:25 +0400

    run tests transactional

--- src/test/java/hexlet/code/controller/TaskControllerIT.java
+++ src/test/java/hexlet/code/controller/TaskControllerIT.java
@@ -30,6 +30,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.List;
@@ -54,6 +55,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @AutoConfigureMockMvc
+@Transactional
 @ActiveProfiles(TEST_PROFILE)
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)

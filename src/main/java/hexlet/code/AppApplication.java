9b5510a101c782e66f86b21bf3783a41a8c1d03a
redgradient <neutral1shade@yahoo.com>
2023-06-01 15:12:06 +0400

    add welcome message on application startup

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,14 +1,48 @@
 package hexlet.code;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static java.util.Optional.ofNullable;
 
 
+@Slf4j
 @SpringBootApplication
 public class AppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        var env = SpringApplication.run(AppApplication.class, args).getEnvironment();
+        logApplicationStartup(env);
     }
 
+    private static void logApplicationStartup(Environment env) {
+        final var protocol = "http";
+        final var port = ofNullable(env.getProperty("server.port")).orElse("8080");
+        final var contextPath = ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(String::isBlank)
+                .orElse("/");
+        var hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        final var msg = """
+
+
+            ----------------------------------------------------------
+                Application '{}' is running! Access URLs:
+                Local:       {}://localhost:{}{}
+                External:    {}://{}:{}{}
+                Profile(s):  {}
+            ----------------------------------------------------------
+            """;
+        final var appName = env.getProperty("spring.application.name");
+        log.info(msg, appName, protocol, port, contextPath, protocol, hostAddress, port, contextPath, env.getActiveProfiles());
+    }
 }
